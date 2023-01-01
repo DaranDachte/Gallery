e60a1aace8b9f009d05f3fbabdfd8cc46791aa5d
Daran Dachte <darandachte@gmail.com>
2023-01-02 00:56:01 +0100

    Gallery text change

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -25,7 +25,7 @@ const Gallery = () => {
       onDoubleClick={() => setActiveCardIndex(false)}
     >
       <Card
-        text={`.Mikhail Baryshnikov
+        text={`Mikhail Baryshnikov
 Born January 28, 1948, is a Soviet Latvian-born Russian-American dancer, choreographer, and actor. He was the preeminent male classical dancer of the 1970s and 1980s. He subsequently became a noted dance director.
 On June 29, 1974, in Toronto while on tour with the Bolshoi, Baryshnikov defected, requesting political asylum in Canada. As recalled by John Frazer, a ballet critic from Toronto who helped Baryshnikov to escape, Frazer wrote down phone numbers of people on a small piece of paper and hid it under his wedding ring. At a banquet after one show he managed to distract the KGB officer who followed Baryshnikov as an interpreter and gave Baryshnikov the paper.Soon he joined the National Ballet of Canada for a brief time in a guest role. He also announced that he would not return to the USSR. He later said that Christina Berlin, an American friend, helped engineer his defection during his 1970 tour of London. His first televised performance after coming out of temporary seclusion in Canada was with the National Ballet of Canada in La Sylphide. He then went to the United States. In December 1975, he and his dance partner Natalia Makarova featured prominently in an episode of the BBC television series Arena.
 In the first two years after his defection, he danced for no fewer than 13 different choreographers, including Jerome Robbins, Glen Tetley, Alvin Ailey, and Twyla Tharp. "It doesn't matter if every ballet is a success or not", he told New York Times dance critic Anna Kisselgoff in 1976. "The new experience gives me a lot." He cited his fascination with the ways Ailey mixed classical and modern technique and his initial discomfort when Tharp insisted he incorporate eccentric personal gestures in dance.`}
@@ -35,7 +35,7 @@ In the first two years after his defection, he danced for no fewer than 13 diffe
         isActive={activeCardIndex === 1}
       />
       <Card
-        text={`6.Niels Bohr
+        text={`Niels Bohr
 7 October 1885  - 18 November 1962) was a Danish physicist who made foundational contributions to understanding atomic structure and quantum theory, for which he received the Nobel Prize in Physics in 1922. Bohr was also a philosopher and a promoter of scientific research.
 
 Bohr developed the Bohr model of the atom, in which he proposed that energy levels of electrons are discrete and that the electrons revolve in stable orbits around the atomic nucleus but can jump from one energy level (or orbit) to another. Although the Bohr model has been supplanted by other models, its underlying principles remain valid. He conceived the principle of complementarity: that items could be separately analysed in terms of contradictory properties, like behaving as a wave or a stream of particles. The notion of complementarity dominated Bohr's thinking in both science and philosophy.
@@ -49,7 +49,7 @@ During the 1930s, Bohr helped refugees from Nazism. After Denmark was occupied b
         isActive={activeCardIndex === 2}
       />
       <Card
-        text={`4.Luis Buñuel
+        text={`Luis Buñuel
  22 February 1900 – 29 July 1983. Was a Spanish-Mexican  filmmaker  who worked in France, Mexico, and Spain. He has been widely considered by many film critics, historians, and directors to be one of the greatest and most influential filmmakers of all time.
 When Buñuel died at age 83, his obituary in The New York Times called him "an iconoclast, moralist, and revolutionary who was a leader of avant-garde surrealism in his youth and a dominant international movie director half a century later". His first picture, Un Chien Andalou—made in the silent era—is still viewed regularly throughout the world and retains its power to shock the viewer,  and his last film, That Obscure Object of Desire—made 48 years later—won him Best Director awards from the National Board of Review and the National Society of Film Critics. Writer Octavio Paz called Buñuel's work "the marriage of the film image to the poetic image, creating a new reality...scandalous and subversive".
 Seven of Buñuel's films are included in Sight & Sound's 2012 critics' poll of the top 250 films of all time. Fifteen of his films are included in the They Shoot Pictures, Don't They? list of the 1,000 greatest films of all time, second only to Jean-Luc Godard, with sixteen, and he ranks number 13 on their list of the top 250 directors.
@@ -61,7 +61,7 @@ Buñuel stayed in Spain for the first few months after the outbreak of the Civil
         isActive={activeCardIndex === 3}
       />
       <Card
-        text={`1. Albert Einstein
+        text={`Albert Einstein
 14 March 1879 - 18 April 1955. Was a German-born theoretical physicist, widely acknowledged to be one of the greatest and most influential physicists of all time. Einstein is best known for developing the theory of relativity, but he also made important contributions to the development of the theory of quantum mechanics. Relativity and quantum mechanics are the two pillars of modern physics. His mass-energy equivalence formula E = mc2, which arises from relativity theory, has been dubbed "the world's most famous equation".  His work is also known for its influence on the philosophy of science. He received the 1921 Nobel Prize in Physics "for his services to theoretical physics, and especially for his discovery of the law of the photoelectric effect",a pivotal step in the development of quantum theory. His intellectual achievements and originality resulted in "Einstein" becoming synonymous with "genius".
 
 Einstein was born in the German Empire, but moved to Switzerland in 1895, forsaking his German citizenship (as a subject of the Kingdom of Württemberg)  the following year.  In 1901, he acquired Swiss citizenship, which he kept for the rest of his life, and in 1903 he secured a permanent position at the Swiss Patent Office in Bern. In 1914, Einstein moved to Berlin in order to join the Prussian Academy of Sciences and the Humboldt University of Berlin. In 1917, Einstein became director of the Kaiser Wilhelm Institute for Physics; he also became a German citizen again, this time Prussian.
@@ -73,7 +73,7 @@ In 1933, while Einstein was visiting the United States, Adolf Hitler came to pow
         isActive={activeCardIndex === 4}
       />
       <Card
-        text={`8.Sigmund Freud
+        text={`Sigmund Freud
 6 May 1856 – 23 September 1939.
 Was an Austrian neurologist and the founder of psychoanalysis, a clinical method for evaluating and treating pathologies explained as originating in conflicts in the psyche, through dialogue between a patient and a psychoanalyst.
 Freud was born to Galician Jewish parents in the Moravian town of Freiberg, in the Austrian Empire. He qualified as a doctor of medicine in 1881 at the University of Vienna.  Upon completing his habilitation in 1885, he was appointed a docent in neuropathology and became an affiliated professor in 1902. Freud lived and worked in Vienna, having set up his clinical practice there in 1886. In 1938, Freud left Austria to escape Nazi persecution. He died in exile in the United Kingdom in 1939.
@@ -85,7 +85,7 @@ Though in overall decline as a diagnostic and clinical practice, psychoanalysis 
         isActive={activeCardIndex === 5}
       />
       <Card
-        text={`9.Victor Hugo
+        text={`Victor Hugo
 26 February 1802 – 22 May 1885
 Was a French Romantic writer and politician. During a literary career that spanned more than sixty years, he wrote in a variety of genres and forms. He is considered to be one of the greatest French writers of all time.
 His most famous works are the novels The Hunchback of Notre-Dame (1831) and Les Misérables (1862). In France, Hugo is renowned for his poetry collections, such as Les Contemplations (The Contemplations) and La Légende des siècles (The Legend of the Ages). Hugo was at the forefront of the Romantic literary movement with his play Cromwell and drama Hernani. Many of his works have inspired music, both during his lifetime and after his death, including the opera Rigoletto and the musicals Les Misérables and Notre-Dame de Paris. He produced more than 4,000 drawings in his lifetime, and campaigned for social causes such as the abolition of capital punishment.
@@ -96,7 +96,7 @@ Though he was a committed royalist when young, Hugo's views changed as the decad
         isActive={activeCardIndex === 6}
       />
       <Card
-        text={`3. Fritz Lang
+        text={`Fritz Lang
 December 5, 1890 – August 2, 1976
 Was an Austrian film director, screenwriter, and producer who worked in Germany and later the United States. One of the best-known émigrés from Germany's school of Expressionism, he was dubbed the "Master of Darkness" by the British Film Institute. He has been cited as one of the most influential filmmakers of all time.
 
@@ -107,7 +107,7 @@ Lang's most celebrated films include the groundbreaking futuristic Metropolis (1
         isActive={activeCardIndex === 7}
       />
       <Card
-        text={` 7.Marlene Dietrich
+        text={`Marlene Dietrich
 27 December 1901 - 6 May 1992
 was a German-born American actress and singer whose career spanned from the 1910s to the 1980s.
 
@@ -120,7 +120,7 @@ Dietrich was known for her humanitarian efforts during World War II, housing Ger
         isActive={activeCardIndex === 8}
       />
       <Card
-        text={`2.Vangelis
+        text={`Vangelis
 29 March 1943 – 17 May 2022
 was a Greek composer and arranger of electronic, progressive, ambient, and classical orchestral music. He was best known for his Academy Award-winning score to Chariots of Fire (1981), as well as for composing scores to the films Blade Runner (1982), Missing (1982), Antarctica (1983), The Bounty (1984), 1492: Conquest of Paradise (1992), and Alexander (2004), and for the use of his music in the 1980 PBS documentary series Cosmos: A Personal Voyage by Carl Sagan.
 
